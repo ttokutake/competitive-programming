1b1b6ed52debdbb97d32b0cee1a91777c5598edf
ttokutake <tadatoshi.tokutake@gmail.com>
2018-05-25 11:10:40 +0900

    Modularize codes

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,7 @@
-use std::io::stdin;
+mod answer;
 
-fn read_i32() -> i32 {
-  let mut s = String::new();
-  stdin().read_line(&mut s).unwrap();
-  s.trim().parse::<i32>().unwrap()
-}
+use answer::q661;
 
 fn main() {
-  let count = read_i32();
-  let mut inputs: Vec<i32> = vec![];
-  for _ in 0..count {
-    let input = read_i32();
-    inputs.push(input);
-  }
-  for input in inputs {
-    if input % 8 == 0 && input % 10 == 0 {
-      println!("ikisugi");
-    } else if input % 8 == 0 {
-      println!("iki");
-    } else if input % 10 == 0 {
-      println!("sugi");
-    } else {
-      println!("{}", input / 3);
-    }
-  }
+  q661::run();
 }
