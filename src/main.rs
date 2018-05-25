69f2f3dfbf1d5c795f8dc107218e033dc1570e0d
ttokutake <tadatoshi.tokutake@gmail.com>
2018-05-25 14:37:28 +0900

    Solve https://yukicoder.me/problems/no/227

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,46 @@
-mod answer;
+// mod answer;
 
-use answer::q661;
+// use answer::q661;
+
+use std::collections::HashMap;
+use std::io::stdin;
+
+fn readln() -> String {
+  let mut s = String::new();
+  stdin().read_line(&mut s).unwrap();
+  s
+}
 
 fn main() {
-  q661::run();
+  let hand = readln()
+    .trim()
+    .split_whitespace()
+    .map(|card| card.parse::<i32>().unwrap())
+    .collect::<Vec<i32>>();
+  let count_map = hand
+    .into_iter()
+    .fold(HashMap::new(), |mut hm: HashMap<i32, i32>, card| {
+      {
+        let entry = hm.entry(card).or_insert(0);
+        *entry += 1;
+      }
+      hm
+    });
+  let count_of_count_map = count_map
+    .values()
+    .fold(HashMap::new(), |mut hm: HashMap<i32, i32>, count| {
+      {
+        let entry = hm.entry(*count).or_insert(0);
+        *entry += 1;
+      }
+      hm
+    });
+
+  match (count_of_count_map.get(&3), count_of_count_map.get(&2)) {
+    (Some(&1), Some(&1)) => println!("FULL HOUSE"),
+    (Some(&1), None    ) => println!("THREE CARD"),
+    (None    , Some(&2)) => println!("TWO PAIR"),
+    (None    , Some(&1)) => println!("ONE PAIR"),
+    _                    => println!("NO HAND"),
+  };
 }
